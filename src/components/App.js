5df73b602927c8c5afe963f9c3e2d2177fd010b9
Kumar Ambuj <kumarambuj1997@gmail.com>
2023-10-11 00:29:37 +0530

    changed

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,33 @@
-import React from 'react'
-import '../styles/App.css';
-const App = () => {
+import React, { useState } from "react";
+import "../styles/App.css";
+export function App() {
+  const [data, setData] = useState([]);
+  const [shape, setShape] = useState("square");
+  function handleChange(e) {
+    setShape(e.target.value);
+    console.log(shape);
+  }
+  function handleClick() {
+    console.log(shape);
+    setData([...data, shape]);
+    setShape(shape);
+  }
 
   return (
     <div id="main">
       <div id="shape-creator">
-        
-
+        <select onChange={handleChange}>
+          <option value={"square"}>Square</option>
+          <option value={"circle"}>Circle</option>
+        </select>
+        <button onClick={handleClick}>Add Shape</button>
       </div>
       <div id="shapes-holder">
+        {data.length > 0 &&
+          data.map((val, index) => {
+            return <div className={val}>{index}</div>;
+          })}
       </div>
     </div>
-  )
+  );
 }
-
-
-export default App;
